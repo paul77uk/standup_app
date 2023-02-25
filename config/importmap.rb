2a88f2b6d0e17e882d4b51f45370208bf412aec2
Paul Vickers <paul77uk@gmail.com>
2023-02-25 15:55:12 +0000

    Added an application layout

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,9 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.1
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stimulus-dropdown" # @2.1.0
+pin "hotkeys-js" # @3.10.1
+pin "stimulus-use" # @0.51.3
