a357b58485c99672c66e4a8f7e50bb6eb2a9c8e3
Paul Vickers <paul77uk@gmail.com>
2023-03-21 14:25:07 +0000

    Devise Install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'activity/mine'
   get 'activity/feed'
+  get 'help/index'
 
   root to: 'activity#mine'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
